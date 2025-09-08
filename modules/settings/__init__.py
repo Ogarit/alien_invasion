e58f9b756a80c2132faab5b77b42017f77274c4b
Tiago Marques Lima <tiagomarqueslima203@gmail.com>
2025-09-08 16:45:28 -0300

    Update Bullets

--- modules/settings/__init__.py
+++ modules/settings/__init__.py
@@ -13,7 +13,7 @@ class Settings():
         self.ship_speed_factor = 1.25
 
         # Configurações dos projéteis
-        self.bullet_speed_factor = 1
-        self.bullet_width = 3
-        self.bullet_weight = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_speed_factor = 1.5
+        self.bullet_width = 2
+        self.bullet_height = 17
+        self.bullet_color = (186, 55, 0)
