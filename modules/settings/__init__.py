ae212ab6066113c4a99f153611ad82edb0009e3e
Tiago Marques Lima <tiagomarqueslima203@gmail.com>
2025-09-08 16:22:36 -0300

    Organizing the modules

--- modules/settings/__init__.py
+++ modules/settings/__init__.py
@@ -7,7 +7,7 @@ class Settings():
         # Configurações da tela
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (10, 23, 41)
 
         # Configurações da espaçonave
         self.ship_speed_factor = 1.5
